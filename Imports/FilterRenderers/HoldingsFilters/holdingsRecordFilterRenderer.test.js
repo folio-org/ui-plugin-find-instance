da95445bd7b90a1d6ffd5c6b997b26b6a63c84d2
cchitneedi <cchitneedi@ebsco.com>
2023-06-06 16:13:05 +0530

    UIPFI-46  create Jest/RTL test for holdingsRecordFilterRenderer.js

---
+++ Imports/FilterRenderers/HoldingsFilters/holdingsRecordFilterRenderer.test.js
@@ -0,0 +1,69 @@
+import '../../../test/jest/__mock__';
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import holdingsRecordFilterRenderer from './holdingsRecordFilterRenderer';
+
+jest.mock('../../imports/HoldingsFilters', () => {
+  return jest.fn(({ onChange, onClear }) => (
+    <div data-testid="mocked-item-filters">
+      Mocked HoldingsRecordFilters Component
+      <button type="button" onClick={() => onClear('filterName')} data-testid="clear-button">
+        Clear
+      </button>
+      <button type="button" onClick={() => onChange({ name: 'filterName', values: ['value1', 'value2'] })} data-testid="change-button">
+        Change
+      </button>
+    </div>
+  ));
+});
+
+const expectedParam = {
+  activeFilters: {
+    state: {
+      filter1: ['value1'],
+      filter2: ['value2'],
+    },
+  },
+  getFilterHandlers: expect.any(Function)
+};
+
+describe('holdingsRecordFilterRenderer', () => {
+  const data = {
+    locations: ['location1', 'location2'],
+    tags: ['tag1', 'tag2'],
+  };
+
+  const onChange = {
+    activeFilters: {
+      state: {
+        filter1: ['value1'],
+        filter2: ['value2'],
+      },
+    },
+    getFilterHandlers: jest.fn(() => ({
+      state: jest.fn(),
+    })),
+  };
+
+  it('renders holdingsRecordFilters component with correct props', () => {
+    const { getByTestId } = render(holdingsRecordFilterRenderer(data)(onChange));
+    const holdingsRecordFiltersComponent = getByTestId('mocked-item-filters');
+    expect(holdingsRecordFiltersComponent).toBeInTheDocument();
+  });
+
+  it('calls onChangeHandler when clear button is clicked', () => {
+    const { getByTestId } = render(holdingsRecordFilterRenderer(data)(onChange));
+    const clearButton = getByTestId('clear-button');
+    userEvent.click(clearButton);
+    expect(onChange).toEqual(expectedParam);
+  });
+
+  it('calls onChangeHandler when change button is clicked', () => {
+    const { getByTestId } = render(holdingsRecordFilterRenderer(data)(onChange));
+    const changeButton = getByTestId('change-button');
+    userEvent.click(changeButton);
+    expect(onChange).toEqual(expectedParam);
+  });
+});
