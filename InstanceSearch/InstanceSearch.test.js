c58519c0a0eab4ceb43ee64e59b8b59d43f87c1c
VSnehalatha <snehalathav@ebsco.com>
2023-06-09 16:59:06 +0530

    create Jest/RTL test for InstanceSearch.js

---
+++ InstanceSearch/InstanceSearch.test.js
@@ -0,0 +1,51 @@
+import '../test/jest/__mock__';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import InstanceSearch from './InstanceSearch';
+
+jest.mock('../hooks/useInstancesQuery', () => {
+  return jest.fn().mockReturnValue([{
+    isLoading: true,
+    data: null,
+  }]).mockReturnValue([{
+    isLoading: false,
+    data: { id: 'instance-1' },
+  }]);
+});
+
+const queryClient = new QueryClient();
+
+const defaultProps = {
+  searchLabel: 'Search',
+  searchButtonStyle: 'style',
+  marginBottom0: true,
+  marginTop0: true,
+  selectInstance: jest.fn(),
+  renderNewBtn: jest.fn(),
+  isMultiSelect: true,
+  onClose: jest.fn(),
+  config:{},
+};
+
+describe('InstanceSearch', () => {
+  test('should render InstanceSearch with no props', () => {
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <InstanceSearch />
+      </QueryClientProvider>
+    );
+    expect(container).toBeInTheDocument();
+  });
+  test('should render InstanceSearch with default props', () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <InstanceSearch {...defaultProps} />
+      </QueryClientProvider>
+    );
+    const searchButton = screen.getByText('Search');
+    userEvent.click(searchButton);
+    expect(defaultProps.onClose).toHaveBeenCalled();
+  });
+});
