e880c10008edb487c6f0b66896403a4b0f9e8ad3
Dmytro-Melnyshyn <77053927+Dmytro-Melnyshyn@users.noreply.github.com>
2024-01-02 18:52:36 +0200

    UIPFI-136: ECS Effective location facet is not showing other member tenants. (#197)

--- Imports/imports/utils.js
+++ Imports/imports/utils.js
@@ -116,3 +116,5 @@ export function getIsbnIssnTemplate(queryTemplate, identifierTypes, queryIndex) 
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
  */
 export const accentFold = (str = '') => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+export const isUserInConsortiumMode = stripes => stripes.hasInterface('consortia');
